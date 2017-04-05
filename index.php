3a9b4965f390c85ea41158caa033c50f8a535be6
markn11 <m-niles@onu.edu>
2017-04-05 08:16:47 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head><b> Hello, World!</b> 
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+
+    <body>
+        
+        What would you like to say back?      
+        <select name="What would you like to say back?">
+            <option>Hello</option>
+            <option>Hola</option>
+            <option>Bonjour</option>
+            <option>Salaam</option>
+            <option>Aloha</option>
+            <option>Hi</option>
+        </select>
+        <div </div>
+        <div </div>
+        <img src="../adv/Screenshot (52).png" alt=""/>
+    </body>
+</html>
